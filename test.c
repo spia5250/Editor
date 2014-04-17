79d242f2e7d04d8a15419b4903f8136a1b816038
spia5250 <lyj12@mobile-os.dankook.ac.kr>
2014-04-17 11:20:27 +0900

    second commit 	modify line number

--- test.c
+++ test.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 
 int main(int argc, char *argv[]){
-	fputs("\033[6;10H",stdout);
-	fputs("\033[6;10HHello\n",stdout);
+	fputs("\033[5;10H",stdout);
+	fputs("\033[5;10HHello\n",stdout);
 
 	return 0;
 }
